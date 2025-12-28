7af0849aab5794dfc982888e5bebf9c7270abc7f
kafnik <kafnik0009000@gamil.com>
2025-12-28 21:17:41 +0500

    Update Master_bot 1.4

--- guess_the_number_bot.py
+++ guess_the_number_bot.py
@@ -1,149 +1,235 @@
 import telebot
 import random
 from time import sleep
-from config import is_admin, ADMIN_USER, TELEGRAM_TOKEN
 from telebot import types
+import configparser
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CFG_PATH = os.path.join(BASE_DIR, "config.cfg")
+
+config = configparser.ConfigParser()
+config.read(CFG_PATH, encoding="utf-8")
+
+if not config.has_section("BOT"):
+    input("❌ config.cfg НЕ НАЙДЕН ИЛИ ПУСТОЙ. Нажми Enter...")
+    exit()
+
+raw_token = config["BOT"]["TOKEN"]
+
+
+bot = telebot.TeleBot(config["BOT"]["TOKEN"])
 
 #---------Главные переменные--------
-VERSION = '1.3'
-error = True
-active_game = False
-events = [ 
-        "Ты нашёл светящуюся тыкву 🎃",
-        "Призрак прошёл сквозь тебя 👻",
-        "Тебя укусил зомби 🧟",
-        "Ведьма угостила зельем 💀",
-        "Ты сбежал от чёрной кошки 🐈‍⬛",
-        "Ты услышал шёпот из темноты 😱",
-        "Ты получил подарок от скелета ☠️",
-        "Ты поймал летучую мышь 🦇"]
-nasty = ["😝Гадость", "🍬Сладость"]
-hearts = 5
-x = 10
+VERSION = '1.4'
+BOT_ENABLED = True
+guess_game_active = False  # Флаг активной игры
+poip = 5
 
-def hallowen(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    tp1 = types.InlineKeyboardButton('🎡Колесо ужасов', callback_data='btn1')
-    tp2 = types.InlineKeyboardButton('🍬Сладость или гадость', callback_data='btn2')
-    markup.add(tp1, tp2)
-    bot.send_message(message.chat.id, f'Вот все игры на Хэллуин', reply_markup=markup)
+#User_name администраторов
+ADMIN_USER = 'Kafnik'
 
-def complet_games(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn1 = types.InlineKeyboardButton('🎡Колесо ужасов', callback_data='btn1')
-    btn2 = types.InlineKeyboardButton('🍬Сладость или гадость', callback_data='btn2')
-    btn3 = types.InlineKeyboardButton('Отгодай число', callback_data='btn3')
+ # Функция проверки администратора
+def is_admin(message):
+    message.from_user.first_name == ADMIN_USER
+    return
+
+def games_menu(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    btn1 = types.InlineKeyboardButton('🎄 Угадай число (1-10)', callback_data='game1')
+    btn2 = types.InlineKeyboardButton('🎡 Новогоднее колесо', callback_data='game2')
+    btn3 = types.InlineKeyboardButton('❄️ Снежинки', callback_data='game3')
     markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, '✅ Игра завершена ! Выберете команду 👇', reply_markup=markup)
+    
+    bot.send_message(message.chat.id, 
+                     '✨ *НОВОГОДНИЕ ИГРЫ* ✨\n\n'
+                     '🎅 Выбери игру и окунись в праздничную атмосферу!',
+                     parse_mode="Markdown", 
+                     reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    msg = bot.send_message(message.chat.id, 'OpenbotAI')
+    if not BOT_ENABLED and not is_admin(message):
+        bot.send_message(message.chat.id, "🚫 Бот временно недоступен.")
+        return
+    
+    msg = bot.send_message(message.chat.id, '<b>OpenbotAI</b>', parse_mode="HTML")
     sleep(1)
-    bot.edit_message_text(f'Привет {message.from_user.first_name}. \nГотов побеждать Тьму ! Если да то напиши /games', message_id=msg.message_id, chat_id=message.chat.id)
-   
-#---------------Меню игр-----------------
-@bot.message_handler(commands=['halloween'])
-def bot_commads_user(message):
-    hallowen(message)
+    
+    bot.edit_message_text(
+        f'Привет, {message.from_user.first_name}! 🎅\n'
+        f'Добро пожаловать в новогодние игры!\n'
+        f'Напиши /games чтобы увидеть игры',
+        message_id=msg.message_id, 
+        chat_id=message.chat.id
+    )
 
 @bot.message_handler(commands=['games'])
-def menu_games(message):
-    if not error:
-     bot.send_message(message.chat.id, '❌У бота ведутся тех.работы не отвлекайте нас. Нам жаль вся информация на канеле: https://t.me/+xtbO7MiUA180NzYy')
+def games_command(message):
+    if not BOT_ENABLED and not is_admin(message):
+        bot.send_message(message.chat.id, "🚫 Бот временно недоступен.")
+        return
+    
+    bot.send_message(
+        message.chat.id,
+        f'✨ *С НОВЫМ ГОДОМ!* ✨\n\n'
+        f'🎅 Добро пожаловать в новогодние игры!',
+        parse_mode="Markdown"
+    )
+    sleep(1)
+    games_menu(message)
+
+@bot.message_handler(commands=["enable"])
+def enable_bot(message):
+    global BOT_ENABLED
+    if not is_admin(message):
+        bot.send_message(message.chat.id, "⛔ У вас нет прав.")
     else:
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        btn1 = types.InlineKeyboardButton('🎡Колесо ужасов', callback_data='btn1')
-        btn2 = types.InlineKeyboardButton('🍬Сладость или гадость', callback_data='btn2')
-        btn3 = types.InlineKeyboardButton('Отгодай число', callback_data='btn3')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Меню игр открыто !', reply_markup=markup)
+        BOT_ENABLED = True
+        bot.send_message(message.chat.id, "✅ Бот включён! 🎄")
+
+@bot.message_handler(commands=["disable"])
+def disable_bot(message):
+    global BOT_ENABLED
+    if not is_admin(message):
+        bot.send_message(message.chat.id, "⛔ У вас нет прав.")
+    else:
+        BOT_ENABLED = False
+        bot.send_message(message.chat.id, "🚫 Бот выключен для всех, кроме админов.")
 
 @bot.callback_query_handler(func=lambda m: True)
 def callback(call):
-    if call.data == 'btn1':
-        num = random.choice(events)
-        bot.send_message(call.message.chat.id, f'🎡*Колесо крутиться...*\n\n {num}', parse_mode="Markdown")
-        sleep(1)
+    if not BOT_ENABLED and call.from_user.first_name not in ADMIN_USER:
+        bot.answer_callback_query(call.id, "🚫 Бот недоступен.")
+        return
+
+    if call.data == 'game1':
+        global guess_game_active
+        guess_game_active = True
+        
+        # Загадываем число
+        secret = random.randint(1, 10)
+        attempts = 5
+        
+        bot.send_message(
+            call.message.chat.id,
+            f'🎄 *ИГРА: УГАДАЙ ЧИСЛО* 🎄\n\n'
+            f'🎅 Я загадал число от 1 до 10\n'
+            f'❤️ У тебя {attempts} попыток\n\n'
+            f'Я загадал: {secret} (только для теста!)\n'
+            f'Введи число:',
+            parse_mode="Markdown"
+        )
         bot.answer_callback_query(call.id)
-        complet_games(call.message)
 
-    elif call.data == 'btn2':
-        num = random.choice(nasty)
-        bot.send_message(call.message.chat.id, f'Тебе выподает: {num}')
-        sleep(1)
+    elif call.data == 'game2':
+        # Новогоднее колесо
+        event = random.choice([
+            "🎅 Встретил Деда Мороза!",
+            "🎄 Украсил ёлку!",
+            "❄️ Слепил снеговика!",
+            "🦌 Покатался на оленях!",
+            "🌟 Поймал звезду!",
+            "🍪 Испеч печенье!"
+        ])
+        
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        btn1 = types.InlineKeyboardButton('🎡 Крутить снова', callback_data='game2')
+        btn2 = types.InlineKeyboardButton('🔙 В меню', callback_data='back_menu')
+        markup.add(btn1, btn2)
+        
+        bot.edit_message_text(
+            f'🎡 *НОВОГОДНЕЕ КОЛЕСО* 🎡\n\n'
+            f'🎅 Колесо крутится...\n\n'
+            f'{event}\n\n'
+            f'✨ Удачного дня!',
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            parse_mode="Markdown",
+            reply_markup=markup
+        )
         bot.answer_callback_query(call.id)
-        complet_games(call.message)
-
-    elif call.data == 'sistem1':
-        error = False
-        bot.send_message(call.message.chat.id, f'Бот переведенн в тех.работы')
-
-    elif call.data == 'btn3':
-        global active_game
-        active_game = True
-        bot.send_message(call.message.chat.id, f'🕷Игра началась! Я загадал число от 1 до {x} \n У тебя есть 5 жизней !')
-
-@bot.message_handler(commands=['sistem'])
-def sistem(message):
-    if not is_admin (message):
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        num1 = types.InlineKeyboardButton('Отключение на техработы', callback_data='sistem1')
-        markup.add(num1)
-        bot.message_message(message.chat.id, f'Выбирите действие:', reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, '❌Эта команда вам не доступна !')
-
-@bot.message_handler(commands=['help'])
-def info_bot(message):
-    if not error(message): 
-        bot.reply_to(message, '❌У бота ведутся тех.работы не отвлекайте нас. Нам жаль вся информация на канеле: https://t.me/+xtbO7MiUA180NzYy')
-    else:
-        bot.send_message(message.chat.id, 'Список команд /start, /games, /help, /helloween')
-
-@bot.message_handler(commands=['info'])
-def info_bot(message):
-    if not is_admin(message): 
-        bot.reply_to(message, '❌У бота ведутся тех.работы не отвлекайте нас. Нам жаль вся информация на канеле: https://t.me/+xtbO7MiUA180NzYy')
-    else:
-        bot.send_message(message.chat.id, f'Бот cделан команиями: OpenbotAI и VECTORBOT \nНо большую чать выполнила комания: OpenbotAI \nЧто есть прикольного, команда /halloween. \nВерсия бота: {VERSION}')
-
+    
+    elif call.data == 'game3':
+        # Игра со снежинками
+        snowflakes = random.randint(5, 20)
+        messages = [
+            f'❄️ Ты поймал {snowflakes} снежинок!',
+            f'🌟 На тебя упало {snowflakes} снежинок!',
+            f'🎄 Собрал {snowflakes} снежинок с ёлки!'
+        ]
+        
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        btn1 = types.InlineKeyboardButton('❄️ Ловить снежинки', callback_data='game3')
+        btn2 = types.InlineKeyboardButton('🔙 В меню', callback_data='back_menu')
+        markup.add(btn1, btn2)
+        
+        bot.edit_message_text(
+            f'❄️ *СНЕЖИНКИ* ❄️\n\n'
+            f'{random.choice(messages)}\n\n'
+            f'✨ Попробуй ещё!',
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            parse_mode="Markdown",
+            reply_markup=markup
+        )
+        bot.answer_callback_query(call.id)
+    
+    elif call.data == 'back_menu':
+        games_menu(call.message)
+        bot.answer_callback_query(call.id)
 
 @bot.message_handler(func=lambda message: True)
-def message_bot_int(message):
-    global hearts, active_game, x
-    if not active_game: 
+def handle_messages(message):
+    global guess_game_active, poip
+
+    if not guess_game_active:
         return
     
     try:
-        guess = int(message.text)
-
-        if 1 <= guess <= x:
-            secret = random.randint(1, x)
-            if guess == secret:
-                bot.reply_to(message, f'🎉Ты победил тьму и угодал число {secret}')
-                active_game = False
-                sleep(1)
-                complet_games(message)
-            else:
-                hearts -= 1
-                if hearts == 0:
-                    bot.reply_to(message, f'💀 Ты проиграл.... Тьма поглотила тебя !\n число {secret}. \n Если хочешь отомсти судьбе ? Напиши /games')
-                    sleep(1)                    
-                    complet_games(message)
-                    hearts = 5
-                    active_game = False
+            guess = int(message.text)
+            
+            if 1 <= guess <= 10:
+                # Создаем новое число для каждой попытки
+                secret = random.randint(1, 10)
+                
+                if guess == secret:
+                    congratulations = [
+                        f'🎉 *УРА! Ты угадал число {secret}!* 🎉\n\n🎅 Отличная работа! С Новым Годом!',
+                        f'🌟 *БРАВО! Число {secret} найдено!* 🌟\n\n🎄 Поздравляю с победой!',
+                        f'🎁 *ВОТ ЭТО ДА! Ты победил!* 🎁\n\n✨ Число {secret} угадано верно!'
+                    ]
+                    bot.reply_to(message, random.choice(congratulations), parse_mode="Markdown")
+                    guess_game_active = False
+                    poip = 5
+                    sleep(1)
+                    markup = types.InlineKeyboardMarkup(row_width=1)
+                    btn11 = types.InlineKeyboardButton('🔢 Угадывать снова', callback_data='game1')
+                    btn22 = types.InlineKeyboardButton('🔙 В меню', callback_data='back_menu')
+                    markup.add(btn11, btn22)
+                    bot.send_message(message.chat.id, 'Желаете сыграть еще ?', reply_markup=markup)
                 else:
-                 bot.reply_to(message, f'💀Осталось жизней: {hearts}')
-        else:
-            bot.reply_to(message, f'🦇Число должно быть 1 до {x}')
-
+                    poip -= 1
+                    
+                    if poip == 0:
+                        bot.reply_to(message, 
+                                   f'❄️ *Попытки закончились* ❄️\n\nБыло загадано число: {secret}\nПопробуй ещё раз!',
+                                   parse_mode="Markdown")
+                        guess_game_active = False
+                        poip = 5
+                        sleep(1)
+                        markup = types.InlineKeyboardMarkup(row_width=1)
+                        btn11 = types.InlineKeyboardButton('🔢 Угадывать снова', callback_data='game1')
+                        btn22 = types.InlineKeyboardButton('🔙 В меню', callback_data='back_menu')
+                        markup.add(btn11, btn22)
+                        bot.send_message(message.chat.id, 'Желаете сыграть еще ?', reply_markup=markup)
+                    else:
+                        hint = "БОЛЬШЕ" if guess > secret else "МЕНЬШЕ"
+                        bot.reply_to(message, f'🎅 Число {hint.lower()}!\n❤️ Осталось попыток: {poip}\nПопробуй ещё!')
+            else:
+                bot.reply_to(message, '🎅 Число должно быть от 1 до 10!')
+                
     except ValueError:
-         bot.reply_to(message, "👻 Это не число... Призраки не понимают слов !'")
+        bot.reply_to(message, "🎅 Пожалуйста, введи число!")
 
-
-
-print('Бот угодай число запущен!')
+print('❄️ Новогодний бот с игрой "Угадай число" запущен! 🎄')
 bot.polling(non_stop=True)
